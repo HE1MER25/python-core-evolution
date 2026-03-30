4d59e26c4050d44a166c113176bc2d367a98cd35
Praise Ajulo-Omojesu <omojayobami@gmail.com>
2026-03-30 23:45:30 +0100

    Lists in python part 1

---
+++ Foundations/lists.py
@@ -0,0 +1,35 @@
+# Lists in Python
+
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(my_list[0])  # Output: 1
+print(my_list[-1])  # Output: 5
+
+# Modifying elements
+my_list[0] = 10
+print(my_list)  # Output: [10, 2, 3, 4, 5]
+
+# Adding elements
+my_list.append(6)
+print(my_list)  # Output: [10, 2, 3, 4, 5, 6]
+
+# Removing elements
+my_list.remove(3)
+print(my_list)  # Output: [10, 2, 4, 5, 6]
+
+# Looping through a list
+for item in my_list:
+    print(item)  
+
+# While loop with list
+i = 0   
+while i < len(my_list):
+    print(my_list[i])
+    i += 1 # Output: 10, 2, 4, 5, 6
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 2 # Output: 10, 4, 6
